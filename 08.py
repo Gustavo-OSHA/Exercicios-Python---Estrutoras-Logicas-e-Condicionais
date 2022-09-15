6bb20f415463eef3bb74c57c7105736178ff0f87
Gustavo de Oliveira Silva Hilario Amorim <105814628+Gustavo-OSHA@users.noreply.github.com>
2022-09-15 14:03:20 -0300

    Update 08.py

--- 08.py
+++ 08.py
@@ -7,32 +7,25 @@ fato deve ser informado ao usuario e o programa termina
 nota_1 = float(input('Digite a nota: '))
 nota_2 = float(input('Digite a segunda nota: '))
 
-def menu_notas():
-    print('Qual a faixa de pontuação? ')
-    print('1. De 1 a 10')
-    print('2. Zero ')
 
-def nota_valida():
-    if nota_1 and nota_2 <=10:
-        print('Nota valida')
+def nota_valida(nota):
+    if nota >= 0 and nota <= 10:
+        return True
+    else:
+        return False  
 
-def nota_invalida():
-    if nota_1 or nota_2 <=0:
-        print('Não é possivel fazer a recuperação')
-
-def media():
+def media(nota_1, nota_2):
     pontuacao = (nota_1 + nota_2) / 2
     print(f'A media é', pontuacao)
 
 if __name__=='__main__':
-    menu_notas()
-    escolha = input('Escolha a nota: ')
 
-    if escolha == '1':
-        media()
+    if (nota_valida(nota_1) and nota_valida(nota_2)):
+        media(nota_1, nota_2)
+    else:
+        print("Pelo menos uma das notas digitadas não é valida. Tente novamente")    
+   
 
-    if  escolha == '2':
-        nota_invalida()  
 
 
     
